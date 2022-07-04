b229df6a37e098fe31fb0af22f89b3a79bc4f986
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2022-07-04 16:57:38 +0800

    fixed minor bug

--- src/NavBar.component.jsx
+++ src/NavBar.component.jsx
@@ -16,8 +16,11 @@ function NavBar() {
                 <path d='M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z' />
             </svg>
             {/* Amount */}
-            <div className='amount-container text-white absolute top-11 left-2'>
-                <p className='total-amount text-xs'>${value}</p>
+            <div className='amount-container text-white absolute top-11 left-1.5'>
+                {value > 0 ? 
+                     <p className='total-amount text-xs'>${value.toFixed(2)}</p> : ''
+                }
+               
             </div>
             {/* Bubble */}
             <div className="bg-blue-100 opacity-90 rounded-full h-8 w-8 flex items-center justify-center absolute right-4 mr-2">
